ba95a20a74a7b96c72963aa881972634c1784ec7
Gilbertkitetu <gilbertkitetu2@gmail.com>
2022-09-26 07:03:34 +0300

    tax calcalculator

---
+++ js/index.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+const netpay = document.querySelector('.netpay')
+const nssfcontribution = document.getElementsByClassName('nssfcontribution').value
+const taxableincome = document.getElementsByClassName('taxableincome').value
+const taxbeforerelief = document.getElementsByClassName('taxbeforerelief').value
+const personalrelief = document.getElementsByClassName('personalrelief').value
+const insurancerelief = document.getElementsByClassName('insurancerelief').value
+const paye = document.getElementsByClassName('paye').value
+const nhifcontribution = document.getElementsByClassName('nhifcontribution').value
+
+window.onload = function() {
+    netpay.innerText = "54555";
+    
+  };
+  netpay.innerText = "5444555";
+
+  function handleGrossSalary () {
+    
+    const grosssalary = document.getElementById('grosssalary').value
+    console.log(grosssalary)
+    console.log("Hrl")
+
+    var nssfcontribution = 1080
+    var personalrelief = 2400
+    
+  }
